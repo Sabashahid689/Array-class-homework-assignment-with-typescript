939f93782ff0aed759c8b6b5e5f4752d5828f56c
Sabashahid689 <163736077+Sabashahid689@users.noreply.github.com>
2024-06-03 18:01:22 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,64 @@
+         // Class Homework with typescript:-
+         // Topic Array with typescript:-
+// Array means different collections of values in array:       
+// 1.By Normal method array:-
+//let arr1 = ["sana khan","Alisbah Ahmed","ummzehra"];
+//console.log("Array 1",arr1);
+// 2.By Constructor array:-
+//let arr2 = new Array("zaineb","rabia");
+//console.log("Array 2",arr2);
+// 3.By Array Method:-
+//let arr3 = Array.of("ferzana","haleema","fatima");
+//console.log("Array 3","arr3");
+// number array:-
+//const numbers: string[] = [1,2,3,4,5,6];
+//console.log(numbers,"numbers");
+// Nested Array:-
+//let arr: (string | string[])[] = [
+   // "alisbah",
+    //"zaineb",
+   // ["ferzana","rabia"],
+//];
+//let nestedArr = arr[2];
+//console.log(nestedArr[0]);
+//console.log(nestedArr[1]);
+//let foods: string[] = ["Biryani","Kabab","Burger","roll"];
+//console.log(foods, "foods before");
+//foods[0] = "pizza";
+//console.log(foods,"foods after");
+// Array push means last se value ko add kerna:-
+//let cars: string[] = ["civics","city","landcruiser"];
+//cars.push ("toyota");
+//cars.push ("ferrari");
+//console.log(cars,"cars");
+// Array pop means last se value ko delete kerna:-
+//let websites: string[]= ["Amazon","Friverr","Canva"];
+//console.log(websites);
+//websites.pop()
+//console.log(websites.pop());
+//console.log(websites);
+// Array shift means starting se value ko delete kerte hai:-
+//let foods:string[] = ["biryani","pizza","fish"];
+//console.log(foods);
+//foods.shift();
+//console.log(foods.shift());
+//console.log(foods);
+// Array unshift means starting se value ko add kerna:-
+//let favoritecountry:string[] = ["USA","Canada","londan"];
+//console.log(favoritecountry);
+//favoritecountry.unshift("Switzerland");
+//console.log(favoritecountry.unshift());
+//console.log(favoritecountry);
+//Array splice means mid se different value ko add kerta hai:-
+//const cities:string[] = ["karachi","lahore","islamabad"];
+//cities.splice(1,0,"multan");
+//console.log(cities,"cities");
+//Array slice method humain ek new array returns kerta hai jis main selected elements hote hai:
+//let cars:string[] = ["city","landcruiser","toyota","civics","ferrari"];
+//let newarr:string[] = cars.slice(1,3);
+//console.log(newarr);
+//console.log("org arr",newarr);
+
+
+
+
